d375e3cd3122e799aa62478d766f711617ecf1f1
Sadman-Wazir <sadmanwazin123@gmail.com>
2024-02-13 19:39:38 +0600

    sadman

---
+++ sam.js
@@ -0,0 +1,22 @@
+package exampleProgram;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+        public static void main(String[] args) {
+
+            int number, remainder;
+            System.out.println("Pls Enter an Integer Number");
+            Scanner scan = new Scanner(System.in);
+            number = scan.nextInt();
+            scan.close();
+
+            remainder number % 2;
+
+            if( remainder == 0)
+                System.out.println(number+" is an Even Number");
+            else
+                System.out.println(number+" is an Odd Number");
+        }
+}
